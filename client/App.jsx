4f9915ba9dd62f7c0797ab596fc0dfd5b3122d0f
Tomusdrw <tomusdrw@gmail.com>
2015-10-04 18:02:15 +0800

    Adding some packages

---
+++ client/App.jsx
@@ -0,0 +1,30 @@
+App = React.createClass({
+  getTasks() {
+    return [
+      { _id: 1, text: "This is task 1" },
+      { _id: 2, text: "This is task 2" },
+      { _id: 3, text: "This is task 3" }
+    ];
+  },
+ 
+  renderTasks() {
+    return this.getTasks().map((task) => {
+      return <div key={task._id}>{task}</div>;
+    });
+  },
+ 
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Self Allocation</h1>
+          <login-buttons />
+        </header>
+ 
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+});
