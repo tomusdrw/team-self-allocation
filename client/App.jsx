ff8a53481cb75b954267a7ed466c098640e7412f
Tomusdrw <tomusdrw@gmail.com>
2015-10-04 18:54:07 +0800

    Displayin form for allocations changing

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,39 @@
 App = React.createClass({
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      currentUser: Meteor.user()
+    }
+  },
+
+  renderData () {
+    if (!this.data.currentUser) {
+      return (
+        <div className="alert alert-warning">
+          Please login or create account first.
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <ProjectAllocations />
+        <hr />
+        <YourAllocations />
+      </div>
+    );
+  },
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Self Allocation</h1>
         </header>
-
-        <ProjectAllocations />
-        <hr />
-        <YourAllocations />
+        
+        {this.renderData()}
       </div>
     );
   }
--- client/YourAllocations.jsx
+++ client/YourAllocations.jsx
@@ -1,10 +1,52 @@
 YourAllocations = React.createClass({
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      currentUser: Meteor.user(),
+      projects: Projects.find({}).fetch()
+    }
+  },
+
+  renderProjectsAsOptions () {
+    return this.data.projects.map((project) => {
+      return (
+        <option value={project._id}>{project.name}</option>
+      );
+    });
+  },
+
+
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Self Allocation</h1>
+          <h1>Your Allocation {this.data.currentUser.name}</h1>
         </header>
+
+        <div className="col-md-5 col-sm-7">
+          <form>
+            <div className="form-group">
+              <label className="form-label">First Preference</label>
+              <select className="form-control">
+                {this.renderProjectsAsOptions()}
+              </select>
+            </div>
+            <div className="form-group">
+              <label className="form-label">Second Preference</label>
+              <select className="form-control">
+                {this.renderProjectsAsOptions()}
+              </select>
+            </div>
+            <div className="form-group">
+              <label className="form-label">Third Preference</label>
+              <select className="form-control">
+                {this.renderProjectsAsOptions()}
+              </select>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
